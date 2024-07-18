de5a0b7cd5d16c1e6f1540087f3c4a681f9c39d0
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-07-18 14:07:31 -0700

    UI: MainView framed out

---
+++ FixEase/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  FixEase
+//
+//  Created by Jeremy Manlangit on 7/17/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
