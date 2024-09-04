2dc0444b978b696a0fbdcb3569606e5a96a20782
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-09-04 10:08:45 -0700

    Add Skip Button to TutorialTabView

--- FixEase/View/TutorialTabView.swift
+++ FixEase/View/TutorialTabView.swift
@@ -18,101 +18,108 @@ struct TutorialTabView: View {
     var body: some View {
         ZStack {
             Color.greenDark.ignoresSafeArea()
-            TabView {
-                // Add Item to Collection...
-                Section {
-                    HStack(spacing: 20) {
-                        ZStack {
-                            Circle()
-                                .frame(width: 78)
-                                .foregroundStyle(Color.white)
-                                .shadow(radius: 10)
-                            Text("+")
-                                .font(.largeTitle.weight(.black))
-                                .foregroundStyle(.greenDark)
-                        }
-                        Group {
-                            VStack(alignment: .leading) {
-                                Text("Start by adding a new ") +
-                                Text("ITEM").fontWeight(.black)
-                                Text("to your ") +
-                                Text("Collection").fontWeight(.black)
-                            }
-                        }
-                            .foregroundStyle(.white)
-                        Spacer()
-                    }
-                    .padding()
-
-                }
-                
-                // Add Upkeeps to Item...
-                Section {
-                    VStack {
+            VStack {
+                Button("Skip") { action() }
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing)
+                Spacer()
+                TabView {
+                    // Add Item to Collection...
+                    Section {
                         HStack(spacing: 20) {
                             ZStack {
                                 Circle()
                                     .frame(width: 78)
                                     .foregroundStyle(Color.white)
                                     .shadow(radius: 10)
-                                Text(upkeep.emoji!)
-                                    .font(.custom("Item Button", fixedSize: 45))
+                                Text("+")
+                                    .font(.largeTitle.weight(.black))
+                                    .foregroundStyle(.greenDark)
                             }
                             Group {
                                 VStack(alignment: .leading) {
-                                    Text("Select an ") +
-                                    Text("ITEM ").fontWeight(.black) +
-                                    Text("you've created")
-                                    Text("and add a new ") +
-                                    Text("UPKEEP").fontWeight(.black)
+                                    Text("Start by adding a new ") +
+                                    Text("ITEM").fontWeight(.black)
+                                    Text("to your ") +
+                                    Text("Collection").fontWeight(.black)
                                 }
                             }
                             .foregroundStyle(.white)
                             Spacer()
                         }
                         .padding()
-                    }
-                }
-                
-                // Completing Tasks...
-                Section {
-                    VStack {
-                        Group {
-                            if !isCompleted {
-                                Text("On the ") +
-                                Text("HOME SCREEN,").fontWeight(.black)
-                                Text(" when tasks are due, you'll see:")
-                            } else {
-                                Text("Marking ") +
-                                Text("COMPLETED ").fontWeight(.black) +
-                                Text("resets the due date according to the cycle created.")
-                            }
-                        }
-                        .foregroundStyle(.white)
-                        UpkeepRow(upkeep, isCompleted: $isCompleted, startWithAnimation: true) { showEndTutorial = true }
-                        Group {
-                            if !isCompleted {
-                                Text("Mark the task when completed.")
-                            }
-                        }
-                        .foregroundStyle(.white)
                         
-                        if showEndTutorial {
-                            Button(action: { action() }, label: {
-                                Text("Continue")
-                                    .bold()
-                                    .frame(maxWidth: .infinity)
-                                    .padding()
-                                    .background(.white.opacity(0.95))
-                                    .foregroundStyle(Color.greenDark)
-                            })
-                            .buttonStyle(.bordered)
+                    }
+                    
+                    // Add Upkeeps to Item...
+                    Section {
+                        VStack {
+                            HStack(spacing: 20) {
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 78)
+                                        .foregroundStyle(Color.white)
+                                        .shadow(radius: 10)
+                                    Text(upkeep.emoji!)
+                                        .font(.custom("Item Button", fixedSize: 45))
+                                }
+                                Group {
+                                    VStack(alignment: .leading) {
+                                        Text("Select an ") +
+                                        Text("ITEM ").fontWeight(.black) +
+                                        Text("you've created")
+                                        Text("and add a new ") +
+                                        Text("UPKEEP").fontWeight(.black)
+                                    }
+                                }
+                                .foregroundStyle(.white)
+                                Spacer()
+                            }
+                            .padding()
                         }
                     }
-                    .padding()
+                    
+                    // Completing Tasks...
+                    Section {
+                        VStack {
+                            Group {
+                                if !isCompleted {
+                                    Text("On the ") +
+                                    Text("HOME SCREEN,").fontWeight(.black)
+                                    Text(" when tasks are due, you'll see:")
+                                } else {
+                                    Text("Marking ") +
+                                    Text("COMPLETED ").fontWeight(.black) +
+                                    Text("resets the due date according to the cycle created.")
+                                }
+                            }
+                            .foregroundStyle(.white)
+                            UpkeepRow(upkeep, isCompleted: $isCompleted, startWithAnimation: true) { showEndTutorial = true }
+                            Group {
+                                if !isCompleted {
+                                    Text("Mark the task when completed.")
+                                }
+                            }
+                            .foregroundStyle(.white)
+                            
+                            if showEndTutorial {
+                                Button(action: { action() }, label: {
+                                    Text("Continue")
+                                        .bold()
+                                        .frame(maxWidth: .infinity)
+                                        .padding()
+                                        .background(.white.opacity(0.95))
+                                        .foregroundStyle(Color.greenDark)
+                                })
+                                .buttonStyle(.bordered)
+                            }
+                        }
+                        .padding()
+                    }
                 }
+                .tabViewStyle(.page)
             }
-            .tabViewStyle(.page)
         }
     }
 }
